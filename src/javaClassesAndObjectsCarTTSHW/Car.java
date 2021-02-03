6331a50f28346dec7a2b0eee692ac5e84c747911
Joey Whalen <jtktwhalen@gmail.com>
2021-02-03 17:03:57 -0500

    initial commit

---
+++ src/javaClassesAndObjectsCarTTSHW/Car.java
@@ -0,0 +1,24 @@
+package javaClassesAndObjectsCarTTSHW;
+
+public class Car {
+	
+	// TODO create a Car Class with 3 private instance variables, 3 constructors, 3 Getters and 3 Setters, Boolean method, String method
+
+	private String make;
+	private String model;
+	private String color;
+	
+	public car() {}
+	
+	public car (String make, String model) {
+		this.make = make;
+		this.model = model;
+	}
+	
+	public car (String make, String model, String color) {
+		this.make = make;
+		this.model = model;
+		this.color = color;
+	}
+
+}
