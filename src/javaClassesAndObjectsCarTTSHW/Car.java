1982477e91b7b9f841ba189ce285243eb43b08aa
Joey Whalen <jtktwhalen@gmail.com>
2021-02-03 17:17:15 -0500

    update Car to include Getters and Setters

--- src/javaClassesAndObjectsCarTTSHW/Car.java
+++ src/javaClassesAndObjectsCarTTSHW/Car.java
@@ -9,16 +9,47 @@ public class Car {
 	private String color;
 	
 	// 3 constructors
-	public car() {}
+	public Car() {}
 	
-	public car (String make, String model) {
+	public Car (String make, String model) {
 		this.make = make;
 		this.model = model;
 	}
 	
-	public car (String make, String model, String color) {
+	public Car (String make, String model, String color) {
 		this.make = make;
 		this.model = model;
+		this.color = color;
+	}
+	
+	// 3 Getters and 3 Setters
+	public String getMake()
+	{
+		return make;
+	}
+	
+	public void setMake(String make)
+	{
+		this.make = make;
+	}
+	
+	public String getModel()
+	{
+		return model;
+	}
+	
+	public void setModel(String model)
+	{
+		this.model = model;
+	}
+	
+	public String getColor()
+	{
+		return color;
+	}
+	
+	public void setColor(String color)
+	{
 		this.color = color;
 	}
 
